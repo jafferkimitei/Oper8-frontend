fc0773d8c44d9e6461ae5a291656dcbb6afd824d
yunghavy <kimutaij192@gmail.com>
2024-11-17 13:32:57 +0300

    fixed axios error

--- src/services/stats-api.js
+++ src/services/stats-api.js
@@ -1,19 +1,13 @@
 import axios from 'axios';
+
 const API_URL = 'https://oper8-backend.onrender.com/stats';
 
 export const fetchStats = async () => {
-    try {
-      const response = await axios.get(`${API_URL}`);
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching stats:', error);
-      throw error;
-    }
-  };
-  
+  try {
+    const response = await axios.get(`${API_URL}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching stats:', error);
+    throw error;
+  }
+};
