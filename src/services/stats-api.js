daa606b9eeba218616606c176b15b6f69b3a93ad
yunghavy <kimutaij192@gmail.com>
2024-11-17 12:59:47 +0300

    callback?

--- src/services/stats-api.js
+++ src/services/stats-api.js
@@ -3,7 +3,7 @@ const API_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const fetchStats = async () => {
     try {
-      const response = await axios.get(`${API_URL}/stats`);
+      const response = await axios.get(`${API_URL}stats`);
   
       if (!response.ok) {
         throw new Error('Network response was not ok');
